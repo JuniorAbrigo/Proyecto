6acff69013ae74cab604aa84f54ade4b1d8402a0
JuniorAbrigo <juabrigoor@uide.edu.ec>
2025-02-03 23:07:45 -0500

    Add files via upload

---
+++ juegoahorcado.py
@@ -0,0 +1,8 @@
+print("Bienvenido al juego del ahorcado!")
+print("Vamos a empezar!")   
+print("Estoy pensando en una palabra de 5 letras")
+print("Tienes 5 intentos para adivinarla")
+print("¡Buena suerte!")
+print("La palabra es:")
+print("_ _ _ _ _")
+print("Ingresa una letra:")
